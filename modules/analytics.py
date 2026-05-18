25f40a4ccaade0fb2ebd4eef38c313e941a95ebb
bjruly <bumjruly06@gmail.com>
2026-05-18 23:37:01 +0700

    18/5/2026-feat: create analytics.py scaffold

---
+++ modules/analytics.py
@@ -0,0 +1,9 @@
+class Analytics:
+    def monthly_summary(self, month: str) -> dict:
+        pass
+    def category_breakdown(self, month: str) -> list[dict]:
+        pass
+    def daily_spending(self, month: str) -> list[dict]:
+        pass
+    def compare_months(self, month1: str, month2: str) -> dict:
+        pass
